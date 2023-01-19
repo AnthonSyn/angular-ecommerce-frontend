69612a329b51678a4cbfdaff13dd095bdddcefce
Anthon <an_2197@hotmail.com>
2023-01-19 15:37:18 -0600

    Adding Security - Login/Logout with okta   -Install of okta dependencies in angular project

---
+++ src/app/components/login-status/login-status.component.ts
@@ -0,0 +1,51 @@
+import { Component, Inject } from '@angular/core';
+import { OktaAuthStateService, OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+
+@Component({
+  selector: 'app-login-status',
+  templateUrl: './login-status.component.html',
+  styleUrls: ['./login-status.component.css']
+})
+export class LoginStatusComponent {
+
+  isAuthenticated: boolean = false;
+  userFullname: string = '';
+
+
+  constructor(private oktaAuthService: OktaAuthStateService,
+              @Inject(OKTA_AUTH) private oktaAuth: OktaAuth){}
+
+
+  ngOnInit(): void{
+
+    // Subscribe to authentication state changes
+    this.oktaAuthService.authState$.subscribe(
+      (result) =>{
+        this.isAuthenticated = result.isAuthenticated!;
+        this.getUserDetails();
+      }
+    );
+
+  }
+
+  getUserDetails() {
+    if(this.isAuthenticated){
+
+      //fetch the logged in user details (user's claims)
+      //
+      // user full name is exposed as a property name
+      this.oktaAuth.getUser().then(
+        (res)=>{
+          this.userFullname = res.name as string;
+        }
+      );
+    }
+  }
+
+  logout(){
+    // terminates the session with Okta and removes current tokens
+    this.oktaAuth.signOut();
+  }
+
+}
---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default  {
+
+  oidc:{
+    clientId: '0oa80lfds6PpahMNH5d7',
+    issuer: 'https://dev-26215425.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}
